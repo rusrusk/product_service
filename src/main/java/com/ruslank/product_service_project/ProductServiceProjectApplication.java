fd4eeb883f26a9b250ac5ef7e79eaf970519d7a6
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-04-26 08:02:35 +0200

    setting up

---
+++ src/main/java/com/ruslank/product_service_project/ProductServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ruslank.product_service_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruslank/product_service_project/ProductServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruslank.product_service_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
